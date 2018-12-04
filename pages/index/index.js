fd198d310df202f218185808c79fe74963eaf41d
hedong <382130287@qq.com>
2018-12-04 10:59:55 +0800

    完成地图组件

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,51 +4,71 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+    latitude: 23.099994,
+    longitude: 113.324520,
+    markers: [{
+        id: 1,
+        latitude: 23.099994,
+        longitude: 113.324520,
+        name: 'T.I.T 创意园'
+      },
+      {
+        id: 2,
+        latitude: 23.099994,
+        longitude: 113.344520,
+        iconPath: '/images/location.png',
+        name: 'T.I.T 创意园'
+      },
+      {
+        id: 3,
+        latitude: 23.099994,
+        longitude: 113.304520,
+        iconPath: '/images/location.png',
+        name: 'T.I.T 创意园'
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    ],
+
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+  onReady: function(e) {
+    this.mapCtx = wx.createMapContext('myMap')
+  },
+  getCenterLocation: function() {
+    this.mapCtx.getCenterLocation({
+      success: function(res) {
+        console.log(res.longitude)
+        console.log(res.latitude)
+      }
     })
-  }
+  },
+  moveToLocation: function() {
+    this.mapCtx.moveToLocation()
+  },
+  translateMarker: function() {
+    this.mapCtx.translateMarker({
+      markerId: 1,
+      autoRotate: true,
+      duration: 1000,
+      destination: {
+        latitude: 23.10229,
+        longitude: 113.3345211,
+      },
+      animationEnd() {
+        console.log('animation end')
+      }
+    })
+  },
+  includePoints: function() {
+    this.mapCtx.includePoints({
+      padding: [10],
+      points: [{
+        latitude: 23.10229,
+        longitude: 113.3345211,
+      }, {
+        latitude: 23.00229,
+        longitude: 113.3345211,
+      }]
+    })
+  },
+
+  onLoad: function() {},
 })
